1eecd122bbe587c2c4c7d4334ba4e848e5ac6f98
Dylan Avery <fossilfuelmusic@gmail.com>
2017-06-18 21:35:34 -0600

    Might write some jest tests in morning, otherwise everything looking good

--- server/server.js
+++ server/server.js
@@ -11,21 +11,21 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 const initialLocations = [
   {
     id: 'id1',
-    name: 'Denver',
-    lat: 39.742043,
-    lng: -104.991531,
-  },
-  {
-    id: 'id2',
     name: 'LA',
     lat: 34.052235,
     lng: -118.243683,
   },
   {
-    id: 'id3',
+    id: 'id2',
     name: 'Boston',
     lat: 42.364506,
     lng: -71.038887,
+  },
+  {
+    id: 'id3',
+    name: 'Denver',
+    lat: 39.742043,
+    lng: -104.991531,
   },
 ];
 
